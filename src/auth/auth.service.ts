8d502383f54d2e7e4c961ee48c6687ed595d4acf
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-10-28 13:00:21 -0500

    :sparkles: added functionality for save details for user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import User from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { UserRoles } from './enums/user.roles.enum';
+import UserDetails from './entities/user.details.entity';
+import { UserTypeDocument } from './enums/user.type.document.enum';
 
 @Injectable()
 export default class AuthService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(UserDetails)
+    private readonly userDetailsRepository: Repository<UserDetails>,
   ) {}
 
   public createUser = async () => {
@@ -17,8 +21,19 @@ export default class AuthService {
       roles: [UserRoles.OWNER],
     });
 
+    const details = this.userDetailsRepository.create({
+      first_name: 'Ernesto',
+      last_name: 'Díaz',
+      documentType: UserTypeDocument.CC,
+      documentNumber: '1082749257',
+      gender: true,
+      birthdate: new Date('1998-01-13'),
+      phone: '3026508102',
+    });
+
     try {
-      await this.userRepository.save(user);
+      details.user = await this.userRepository.save(user);
+      await this.userDetailsRepository.save(details);
     } catch (error) {
       console.log(error);
     }
