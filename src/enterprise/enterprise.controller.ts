b5746233370f75268d904829f132dedb7fd6e5db
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-10-31 13:16:27 -0500

    :sparkles: require auth with rol Admin for create enterprise and user

--- src/enterprise/enterprise.controller.ts
+++ src/enterprise/enterprise.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseFilters } from '@nestjs/common';
 import EnterpriseService from './enterprise.service';
 import CreateEnterpriseDTO from './dto/create-enterprise.dto';
 import TypeormExceptionFilter from '../common/exceptions/typeorm.exception';
+import Auth from '../auth/decorators/auth.decorator';
+import { UserRoles } from '../auth/enums/user.roles.enum';
 
 @Controller('enterprise')
 @UseFilters(TypeormExceptionFilter)
@@ -9,6 +11,7 @@ export default class EnterpriseController {
   constructor(private readonly enterpriseService: EnterpriseService) {}
 
   @Post()
+  @Auth(UserRoles.SUPER_ADMIN)
   public createEnterpriseAndUser(@Body() dto: CreateEnterpriseDTO) {
     return this.enterpriseService.createEnterpriseAndUser(dto);
   }
