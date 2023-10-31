783d4d1cbdc1e670bab80299442419309e8afc29
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-10-31 11:37:07 -0500

    :sparkles: create new function for login user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, UseFilters } from '@nestjs/common';
+import { Body, Controller, Post, UseFilters } from '@nestjs/common';
 import TypeormExceptionFilter from '../common/exceptions/typeorm.exception';
+import LoginUserDto from './dto/login.dto';
+import AuthService from './auth.service';
 
 @Controller('auth')
 @UseFilters(TypeormExceptionFilter)
-export default class AuthController {}
+export default class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('login')
+  public login(@Body() dto: LoginUserDto) {
+    return this.authService.login(dto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import User from './entities/user.entity';
 import { DataSource, Repository } from 'typeorm';
 import ErrorDatabaseService from '../common/service/error.database.service';
+import JwtPayload from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+import LoginUserDto from './dto/login.dto';
 
 @Injectable()
 export default class AuthService {
@@ -10,5 +13,20 @@ export default class AuthService {
     private dataSource: DataSource,
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     private readonly errorDatabaseService: ErrorDatabaseService,
+    private readonly jwtService: JwtService,
   ) {}
+
+  private generateJWT = (payload: JwtPayload) => {
+    return this.jwtService.sign(payload);
+  };
+
+  public login = async ({ email, password }: LoginUserDto) => {
+    const userFound = await this.userRepository.findOne({
+      where: { email },
+      select: ['email', 'password'],
+    });
+    if (!userFound) throw new NotFoundException('Credenciales no encontradas');
+    console.log(userFound);
+    return userFound;
+  };
 }
