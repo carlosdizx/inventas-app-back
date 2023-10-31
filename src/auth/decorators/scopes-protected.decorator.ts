c9ead3807e5528528486a88813082998362e83b1
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-10-31 15:46:25 -0500

    :sparkles: create decorator for get enterprise

--- src/auth/decorators/scopes-protected.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Scopes } from '../enums/scopes.enum';
-
-export const META_SCOPES = 'scopes';
-const scopesProtected = (...args: Scopes[]) => {
-  return SetMetadata(META_SCOPES, args);
-};
-
-export default scopesProtected;
--- src/auth/user.crud.controller.ts
+++ src/auth/user.crud.controller.ts
@@ -11,6 +11,10 @@ import TypeormExceptionFilter from '../common/exceptions/typeorm.exception';
 import CreateUserDto from './dto/create-user.dto';
 import UserCrudService from './user.crud.service';
 import UpdateUserDto from './dto/update-user.dto';
+import getEnterprise from './decorators/get-enterprise.decorator';
+import Enterprise from '../enterprise/entities/enterprise.entity';
+import Auth from './decorators/auth.decorator';
+import { UserRoles } from './enums/user.roles.enum';
 
 @Controller('users')
 @UseFilters(TypeormExceptionFilter)
@@ -22,10 +26,13 @@ export default class UserCrudController {
   }
 
   @Patch(':id')
+  @Auth(UserRoles.OWNER)
   public async changeStatus(
     @Body() dto: UpdateUserDto,
     @Param('id', ParseUUIDPipe) id: string,
+    @getEnterprise() enterprise: Enterprise,
   ) {
+    console.log(enterprise);
     return { id, ...dto };
   }
 }
