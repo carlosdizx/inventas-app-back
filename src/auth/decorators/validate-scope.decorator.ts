93fe36244d1323ff64996f37eb071e6d55a7db29
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-10-31 12:40:06 -0500

    :fire: remove unnecessary files

--- src/auth/decorators/validate-scope.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { applyDecorators, UseGuards } from '@nestjs/common';
-import { Scopes } from '../enums/scopes.enum';
-import scopesProtected from './scopes-protected.decorator';
-import ScopeGuard from '../guards/scope.guard';
-
-const ValidateScope = (...scopes: Scopes[]) =>
-  applyDecorators(scopesProtected(...scopes), UseGuards(ScopeGuard));
-
-export default ValidateScope;
