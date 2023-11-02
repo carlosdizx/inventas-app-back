0229b476714d4f35c603818d05c8481dd82d7b92
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-11-01 23:09:33 -0500

    :sparkles: inject repository in service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import Product from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export default class ProductsService {}
+export default class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  public createProduct = () => {};
+}
