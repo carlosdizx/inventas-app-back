aff295e597cb58d8f38b3b78986de1b90ba3af73
ernesto diaz <ernesto.diaz@miliopay.com>
2023-12-03 12:27:11 -0500

    :sparkles: validate array for roles min value should be 1

--- src/auth/dto/update-user.dto.ts
+++ src/auth/dto/update-user.dto.ts
@@ -1,10 +1,11 @@
 import { StatusEntity } from '../../common/enums/status.entity.enum}';
-import { IsArray, IsEnum, IsOptional } from 'class-validator';
+import { ArrayMinSize, IsArray, IsEnum, IsOptional } from 'class-validator';
 import { UserRoles } from '../enums/user.roles.enum';
 
 export default class UpdateUserDto {
   @IsOptional()
   @IsArray()
+  @ArrayMinSize(1)
   @IsEnum(UserRoles, { each: true })
   roles?: UserRoles[];
 
