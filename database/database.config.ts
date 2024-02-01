372d9897caea751fbcd801bab426e4a63e48abaa
ernesto diaz <ernesto.diaz@miliopay.com>
2024-01-31 21:30:53 -0500

    :sparkles: added two configs for database with typeorm

--- database/database.config.ts
+++ database/database.config.ts
@@ -1,9 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
 
-const getTypeOrmConfig = async (
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const getTypeOrmConfig = (
   configService: ConfigService,
-): Promise<TypeOrmModuleOptions> => ({
+): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: configService.get('DB_HOST'),
   port: configService.get('DB_PORT'),
@@ -15,4 +20,27 @@ const getTypeOrmConfig = async (
   migrationsTableName: 'migrations',
   synchronize: configService.get('DB_SYNCHRONIZE', false),
 });
-export default getTypeOrmConfig;
+console.log('password', process.env.DB_PASSWORD);
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '../../**/*.migration{.ts,.js}'],
+  migrationsTableName: 'migrations',
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
+export default AppDataSource;
