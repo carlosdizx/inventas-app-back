80476421fbde211cca4136ca76fcb370c010b346
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-03 12:28:10 -0500

    :fire: remove password in dto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -5,10 +5,6 @@ import {
   IsEmail,
   IsEnum,
   IsNotEmpty,
-  IsOptional,
-  Matches,
-  MaxLength,
-  MinLength,
 } from 'class-validator';
 import { UserRoles } from '../enums/user.roles.enum';
 import { documentTypes } from '../../common/enums/document.type.enum';
@@ -16,16 +12,6 @@ import { documentTypes } from '../../common/enums/document.type.enum';
 export default class CreateUserDto {
   @IsEmail()
   email: string;
-
-  @IsOptional()
-  @IsNotEmpty({ message: 'Password no puede estar vació' })
-  @MinLength(6, { message: 'Mínimo de caracteres es 6' })
-  @MaxLength(50, { message: 'Máximo de caracteres es 50' })
-  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message:
-      'El password debería tener una mayúscula, una minúscula y un numero',
-  })
-  password?: string;
 
   @IsArray()
   @IsEnum(UserRoles, { each: true })
