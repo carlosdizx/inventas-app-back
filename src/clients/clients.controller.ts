227c3819c8fca60830ca2390117d7d8ba27c925a
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-24 17:23:18 -0500

    :sparkles: create function for list clients

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import ClientsService from './clients.service';
+import Auth from '../auth/decorators/auth.decorator';
+import PaginationDto from '../common/dto/pagination.dto';
+import getDataReq from '../auth/decorators/get-data-req.decorator';
+import Enterprise from '../enterprise/entities/enterprise.entity';
 
 @Controller('clients')
 export default class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
+
+  @Get()
+  @Auth()
+  public listCategories(
+    @Query() { page, limit }: PaginationDto,
+    @getDataReq() enterprise: Enterprise,
+  ) {
+    return this.clientsService.listClients({ page, limit }, enterprise);
+  }
 }
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import Client from './entities/client.entity';
+import { Repository } from 'typeorm';
+import { IPaginationOptions, paginate } from 'nestjs-typeorm-paginate';
+import Enterprise from '../enterprise/entities/enterprise.entity';
 
 @Injectable()
-export default class ClientsService {}
+export default class ClientsService {
+  constructor(
+    @InjectRepository(Client)
+    private readonly clientRepository: Repository<Client>,
+  ) {}
+
+  public listClients = async (
+    { page, limit }: IPaginationOptions,
+    { id }: Enterprise,
+  ) => {
+    const queryBuilder = this.clientRepository
+      .createQueryBuilder('client')
+      .where('client.enterprise.id = :id', { id });
+    return await paginate<Client>(queryBuilder, {
+      page,
+      limit,
+      route: 'clients',
+    });
+  };
+}
