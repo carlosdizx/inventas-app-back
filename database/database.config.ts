6c97926e67b7393966b9b53577234203428e231f
ernesto diaz <ernesto.diaz@miliopay.com>
2024-07-17 10:40:22 -0500

    :sparkles: usage url for connect to database

--- database/database.config.ts
+++ database/database.config.ts
@@ -4,11 +4,7 @@ dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URL,
   autoLoadEntities: true,
   // entities: [__dirname + '../../**/*.entity{.ts,.js}'],
   migrations: ['dist/database/migrations/*.js'],
