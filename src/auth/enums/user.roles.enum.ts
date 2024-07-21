2ca14ccd26d31947508faf226e352d8acb938219
ernesto diaz <ernesto.diaz@miliopay.com>
2024-07-21 11:15:54 -0500

    :sparkles: register new role

--- src/auth/enums/user.roles.enum.ts
+++ src/auth/enums/user.roles.enum.ts
@@ -3,4 +3,18 @@ export enum UserRoles {
   OWNER,
   CASHIER,
   ACCOUNTANT,
+  ADMIN,
 }
+
+export const ONLY_ROLES_ENTERPRISES: UserRoles[] = [
+  UserRoles.OWNER,
+  UserRoles.ADMIN,
+  UserRoles.ACCOUNTANT,
+  UserRoles.CASHIER,
+];
+
+export const ALL_ROLES_FOR_ENTERPRISE = (): UserRoles[] => {
+  return Object.values(UserRoles)
+    .filter((userRole) => typeof userRole === 'number')
+    .filter((userRole) => userRole !== 0) as UserRoles[];
+};
