7e8ea6326b8fed5a6b00009faabe90397895d7ca
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-07-21 02:26:31 -0500

    :sparkles: add new seed for plans
    Signed-off-by: ernestodiaz <carlos.ernesto.diaz.98@gmail.com>

--- database/migrations/1721544980376-seed.ts
+++ database/migrations/1721544980376-seed.ts
@@ -6,43 +6,71 @@ export class Seed1721544980376 implements MigrationInterface {
       .createQueryBuilder()
       .insert()
       .into('plans')
-      .values([
-        {
-          name: 'Básico',
-          description:
-            'Plan diseñado para empresas que están comenzando, con hasta 3 usuarios.',
-          max_users: 3,
-        },
-        {
-          name: 'Estándar',
-          description:
-            'Ideal para empresas en crecimiento que requieren hasta 5 usuarios.',
-          max_users: 5,
-        },
-        {
-          name: 'Avanzado',
-          description:
-            'Perfecto para empresas medianas con necesidades de hasta 10 usuarios.',
-          max_users: 10,
-        },
-        {
-          name: 'Premium',
-          description: 'Para grandes empresas que necesitan hasta 15 usuarios.',
-          max_users: 15,
-        },
-        {
-          name: 'Pro',
-          description:
-            'Plan avanzado para medianas empresas con hasta 30 usuarios.',
-          max_users: 30,
-        },
-        {
-          name: 'Full',
-          description:
-            'El mejor plan para mediana-grandes empresas, soportando hasta 50 usuarios.',
-          max_users: 50,
-        },
-      ])
+      .values({
+        name: 'Básico',
+        description:
+          'Plan diseñado para empresas que están comenzando, con hasta 3 usuarios.',
+        max_users: 3,
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into('plans')
+      .values({
+        name: 'Estándar',
+        description:
+          'Ideal para empresas en crecimiento que requieren hasta 5 usuarios.',
+        max_users: 5,
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into('plans')
+      .values({
+        name: 'Avanzado',
+        description:
+          'Perfecto para empresas con necesidades de hasta 10 usuarios.',
+        max_users: 10,
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into('plans')
+      .values({
+        name: 'Premium',
+        description: 'Para empresas que necesitan hasta 15 usuarios.',
+        max_users: 15,
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into('plans')
+      .values({
+        name: 'Pro',
+        description:
+          'Plan avanzado para empresas medianas con hasta 30 usuarios.',
+        max_users: 30,
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into('plans')
+      .values({
+        name: 'Full',
+        description:
+          'El mejor plan para empresas medianas o grandes, soportando hasta 50 usuarios.',
+        max_users: 50,
+      })
       .execute();
   }
 
@@ -52,14 +80,7 @@ export class Seed1721544980376 implements MigrationInterface {
       .delete()
       .from('plans')
       .where('name IN (:...names)', {
-        names: [
-          'Básico',
-          'Estándar',
-          'Avanzado',
-          'Premium',
-          'Premium Pro',
-          'Premium Pro Black',
-        ],
+        names: ['Básico', 'Estándar', 'Avanzado', 'Premium', 'Pro', 'Full'],
       })
       .execute();
   }
