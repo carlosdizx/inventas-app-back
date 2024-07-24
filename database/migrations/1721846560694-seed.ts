6ed98c59642ff8b423813dd485160c251cea7455
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-07-24 13:56:35 -0500

    :sparkles: create new seed for replace super admin in app
    Signed-off-by: ernestodiaz <carlos.ernesto.diaz.98@gmail.com>

---
+++ database/migrations/1721846560694-seed.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Seed1721846560694 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .update('users')
+      .set({ email: 'superadmin.inventas@yopmail.com' })
+      .where('email = :email', { email: 'demo.inventas@yopmail.com' })
+      .execute();
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .update('users')
+      .set({ email: 'demo.inventas@yopmail.com' })
+      .where('email = :email', { email: 'superadmin.inventas@yopmail.com' })
+      .execute();
+  }
+}
