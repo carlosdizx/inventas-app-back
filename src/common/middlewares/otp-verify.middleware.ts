a18546b18595d379692ddd6f95b72199a66fcf51
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-07-25 00:34:32 -0500

    :sparkles: create middleware for verify otp in request
    Signed-off-by: ernestodiaz <carlos.ernesto.diaz.98@gmail.com>

---
+++ src/common/middlewares/otp-verify.middleware.ts
@@ -0,0 +1,34 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NestMiddleware,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import User from '../../auth/entities/user.entity';
+import { AUTH, SERVER } from '../constants/messages.constant';
+import OtpService from '../service/otp.service';
+@Injectable()
+export class OtpVerifyMiddleware implements NestMiddleware {
+  private readonly logger: Logger = new Logger(OtpVerifyMiddleware.name);
+
+  constructor(private readonly otpService: OtpService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    this.logger.debug('Verify otp');
+
+    const user = req['user'] as User;
+    if (!user) throw new InternalServerErrorException(SERVER.FATAL);
+
+    const { email } = user;
+    const otp = req.headers['x-otp-code'] as string;
+
+    if (!otp) throw new BadRequestException(AUTH.INVALID);
+
+    this.logger.debug(`email: ${email}`);
+
+    await this.otpService.verifyOtp(email, otp);
+
+    next();
+  }
+}
