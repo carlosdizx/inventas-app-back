3291fcb75f871c3d608587cda056f89dd8afbf88
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-07-24 19:20:54 -0500

    :bug: change logic and remove transaction
    Signed-off-by: ernestodiaz <carlos.ernesto.diaz.98@gmail.com>

--- database/migrations/1721865364073-mig.ts
+++ database/migrations/1721865364073-mig.ts
@@ -1,32 +1,22 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Mig1721865364073 implements MigrationInterface {
+export class CreateExtensionAndSetTimeZone1721865364073
+  implements MigrationInterface
+{
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.startTransaction();
     try {
       await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
       await queryRunner.query(`SET TIME ZONE 'America/Bogota'`);
-
-      await queryRunner.commitTransaction();
     } catch (error) {
-      await queryRunner.rollbackTransaction();
       throw error;
-    } finally {
-      await queryRunner.release();
     }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.startTransaction();
     try {
       await queryRunner.query(`SET TIME ZONE 'UTC'`);
-
-      await queryRunner.commitTransaction();
     } catch (error) {
-      await queryRunner.rollbackTransaction();
       throw error;
-    } finally {
-      await queryRunner.release();
     }
   }
 }
