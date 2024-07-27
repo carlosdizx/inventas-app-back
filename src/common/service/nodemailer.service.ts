3124f7f386d448fc9eb1a1906b33012cece2ef97
ernesto diaz <ernesto.diaz@miliopay.com>
2024-07-26 21:05:09 -0500

    :sparkles: create template for send otp notification to user for confirm actions

--- src/common/service/nodemailer.service.ts
+++ src/common/service/nodemailer.service.ts
@@ -1,8 +1,13 @@
 import * as nodemailer from 'nodemailer';
 import { ConfigService } from '@nestjs/config';
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import SendEmailDto from '../dto/send.email.dto';
 import { SentMessageInfo } from 'nodemailer/lib/smtp-transport';
+import { SERVER } from '../constants/messages.constant';
 
 @Injectable()
 export default class NodemailerService {
@@ -30,11 +35,18 @@ export default class NodemailerService {
       .then(() => this.logger.debug('Connected to email successfully'));
   }
 
-  public main = async ({ from, to, subject, html }: SendEmailDto) =>
-    await this.transporter.sendMail({
-      from,
-      to,
-      subject,
-      html,
-    });
+  public main = async ({ from, to, subject, html }: SendEmailDto) => {
+    this.logger.debug(`Sending email to: ${to}`);
+    try {
+      await this.transporter.sendMail({
+        from,
+        to,
+        subject,
+        html,
+      });
+    } catch (error) {
+      this.logger.log(error);
+      throw new InternalServerErrorException(SERVER.FATAL);
+    }
+  };
 }
