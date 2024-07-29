afe474452a5ef34ecadcf0af816db2568891c182
ernesto diaz <ernesto.diaz@miliopay.com>
2024-07-29 15:19:47 -0500

    :sparkles: add validation for only generate documentation in develop

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { LogLevel } from '@nestjs/common/services/logger.service';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 const bootstrap = async () => {
   const configService = new ConfigService();
 
-  const logLevel: LogLevel[] =
-    configService.get('NODE_ENV') === 'production'
-      ? ['error', 'warn', 'fatal']
-      : ['error', 'warn', 'log', 'debug', 'fatal', 'verbose'];
+  const env = configService.get('NODE_ENV', 'development') === 'production';
+
+  const logLevel: LogLevel[] = env
+    ? ['error', 'warn', 'fatal']
+    : ['error', 'warn', 'log', 'debug', 'fatal', 'verbose'];
 
   const app: INestApplication = await NestFactory.create(AppModule);
 
@@ -24,23 +26,27 @@ const bootstrap = async () => {
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Documentación Inventas App')
-    .setDescription(
-      'La API de Inventas App proporciona servicios para gestionar usuarios, productos, y más.',
-    )
-    .setVersion('1.0')
-    .addTag('Inventas App')
-    .setContact(
-      'Ernesto Díaz Basante',
-      'https://github.com/carlosdizx',
-      'inventasapp@gmail.com',
-    )
-    .setLicense('MIT', 'https://opensource.org/licenses/MIT')
-    .build();
+  if (!env) {
+    const config = new DocumentBuilder()
+      .setTitle('Documentación Inventas App')
+      .setDescription('Gestiona usuarios, productos, ventas y más.')
+      .setVersion('1.0')
+      .addTag('Inventas App')
+      .setContact(
+        'Ernesto Díaz Basante',
+        'https://github.com/carlosdizx',
+        'inventasapp@gmail.com',
+      )
+      .setLicense('MIT', 'https://opensource.org/licenses/MIT')
+      .addBearerAuth()
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+
+    fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
+
+    SwaggerModule.setup('documentation', app, document);
+  }
 
   const port = configService.getOrThrow<string>('APP_PORT');
   await app.listen(port);
