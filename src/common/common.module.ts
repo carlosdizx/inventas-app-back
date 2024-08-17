3de796d3bec758eeda10c8ad3371d7576d38f858
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-08-16 19:38:15 -0500

    :fire: **remove cache service**
    Signed-off-by: ernestodiaz <carlos.ernesto.diaz.98@gmail.com>

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,23 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import ErrorDatabaseService from './service/error.database.service';
 import EncryptService from './service/encrypt.service';
 import NodemailerService from './service/nodemailer.service';
-import { CacheModule } from '@nestjs/cache-manager';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import FirebaseService from './service/firebase.service';
 import FirestoreService from './service/firestore.service';
 import OtpService from './service/otp.service';
 
 @Global()
 @Module({
-  imports: [
-    CacheModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        ttl: parseInt(configService.get('CACHE_TTL')),
-      }),
-    }),
-  ],
   providers: [
     ErrorDatabaseService,
     EncryptService,
