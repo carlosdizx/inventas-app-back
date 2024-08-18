a5099989ed7ecb94a77eadec6cc17bb9b3b97ec2
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-08-17 23:04:38 -0500

    :sparkles: create guard for usage logic for otp
    Signed-off-by: ernestodiaz <carlos.ernesto.diaz.98@gmail.com>

---
+++ src/users/user-otp.service.ts
@@ -0,0 +1,53 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import UserOtp from './entities/user-otp.entity';
+import { Repository } from 'typeorm';
+import { OTP } from '../common/constants/messages.constant';
+import generateNumberCodeUtil from '../common/util/generate-number-code.util';
+
+@Injectable()
+export default class UserOtpService {
+  constructor(
+    @InjectRepository(UserOtp)
+    private readonly otpRepository: Repository<UserOtp>,
+  ) {}
+
+  public generateOtp = async (userId: string) => {
+    const otp = generateNumberCodeUtil(6);
+    const expiresAt = new Date(Date.now() + 10 * 60000);
+
+    let userOtp = await this.otpRepository.findOne({
+      where: { user: { id: userId } },
+    });
+
+    if (userOtp) {
+      userOtp.otp = otp;
+      userOtp.expiresAt = expiresAt;
+    } else {
+      userOtp = this.otpRepository.create({
+        otp,
+        expiresAt,
+        user: { id: userId },
+      });
+    }
+
+    await this.otpRepository.save(userOtp);
+    return { message: OTP.SUCCESS };
+  };
+
+  public validateOtp = async (
+    userId: string,
+    otp: string,
+  ): Promise<boolean> => {
+    const userOtp = await this.otpRepository.findOne({
+      where: { user: { id: userId }, otp },
+    });
+
+    if (!userOtp || userOtp.expiresAt < new Date())
+      throw new BadRequestException(`${OTP.EXPIRED} o ${OTP.NOT_FOUND}`);
+
+    await this.otpRepository.remove(userOtp);
+
+    return true;
+  };
+}
